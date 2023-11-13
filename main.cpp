17b52430a70725ccd8b81a74067c72f34317af72
loonek <28lunek@wp.pl>
2023-11-13 03:55:19 +0100

    it kinda works
    some todos, and customization to be added

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ struct Chromosome {
 // Function to initialize population
 std::vector<Chromosome> initializePopulation(int populationSize, int numCities){
     std::vector<Chromosome> population(populationSize);
-    for(int i = 0; i < populationSize){
+    for(int i = 0; i < populationSize; i++){
         population[i].path.resize(numCities);
         for(int j = 0; j < numCities; j++){
             population[i].path[j] = j; // Initializing with path 0, 1, 2 ... numCities - 1
@@ -25,10 +25,10 @@ std::vector<Chromosome> initializePopulation(int populationSize, int numCities){
 }
 
 // Function to calculate distance of a path
-int CalculateDistance(const std::vector<int>& path, const std::vector<std::vector<int>>& distanceMatrix){
+int calculateDistance(const std::vector<int>& path, const std::vector<std::vector<int>>& distanceMatrix){
     int distance = 0;
     int numCities = path.size();
-    for(int i = 0; i < numCities; i++){
+    for(int i = 0; i < numCities - 1; i++){
         distance += distanceMatrix[path[i]][path[i + 1]];
     }
     distance += distanceMatrix[path[numCities - 1]][path[0]]; // Return to the starting city
@@ -40,7 +40,7 @@ Chromosome crossover(const Chromosome& parent1, const Chromosome& parent2){
     int numCities = parent1.path.size(); // Initializing number of cities in this function
     Chromosome child; // Initializing a child
     int crossoverPoint = std::rand() % (numCities - 1) + 1; // Randomly choosing crossover point, excluding first city
-    std::copy_n(parent1.path.begin(), crossoverPoint, std::back_insterter(child.path));
+    std::copy_n(parent1.path.begin(), crossoverPoint, std::back_inserter(child.path));
     std::copy_if(parent2.path.begin(), parent2.path.end(), std::back_inserter(child.path), [child](int city){ // Functions that copies rest of the path from second parent
                 return std::find(child.path.begin(), child.path.end(), city) == child.path.end(); // Condition, that check whether this city wasn't already in child's path 
                 }); // Gonna be completety honest, this solution was achieved with a lot of outside help
@@ -60,13 +60,17 @@ void mutate(Chromosome& chromosome){
 int main(){
     srand(time(0));
     
-    int populationSize = 50;
-    int numCities = 5;
-    int generations = 30;
+    int populationSize = std::rand() % 40 + 10;
+    int numCities = std::rand() % 50 + 1;
+    int generations = std::rand() % 1000 + 1;
+
+    std::cout << "Population size = " << populationSize << std::endl;
+    std::cout << "Number of cities = " << numCities << std::endl;
+    std::cout << "Number of generations = " << generations << std::endl;
 
     std::vector<std::vector<int>> distanceMatrix(numCities, std::vector<int>(numCities, 0)); // Initialization of a "map"
 
-    for (int i = 0; i < numCities; ++i) {
+    for (int i = 0; i < numCities; ++i) { // TODO make matrix customizable, and ask about amount of cities
         for (int j = 0; j < numCities; ++j) {
             if (i != j) {
                 distanceMatrix[i][j] = rand() % 91 + 10; // Assignes random distance between 10 and 100
@@ -74,24 +78,58 @@ int main(){
         }
     }
 
-    // Placeholder for initialization of population
+    // Initializing population
+    std::vector<Chromosome> population = initializePopulation(populationSize, numCities);
 
     // Main loop
     for(int generation = 0; generation < generations; generation++){
-        //Calculate the fitness of generation
+        // Calcute fitness
+        for(Chromosome& chromosome : population){ // Loop iterates for every chromosome int he population
+            if(chromosome.fitness == 0){
+                chromosome.fitness = calculateDistance(chromosome.path, distanceMatrix);
+            }
+        }
 
         // Sort population based on fitness
+        for(int i = 0; i < populationSize; i++){ // There's propably a better way to do it
+            for(int j = i; j < populationSize; j++){
+                if(population[i].fitness > population[j].fitness){
+                    std::swap(population[i], population[j]);
+                }
+            }
+        }
 
         // Display best fitness
+        std::cout<< "Generation = " << generation + 1<< "   Best path length = " <<population[0].fitness << std::endl;
 
         // Select top 10% (elite) of population to breed
+        int eliteSize = populationSize / 10; // For this to work population size has to greater than 10, TODO find a fix
+        std::vector<Chromosome> elite(population.begin(), population.begin() + eliteSize);
 
-        // Breed and mutate
-
+        // Breed and mutate offsprings
+        std::vector<Chromosome> offspring;
+        for(int i = 0; i < populationSize - populationSize/10; i++){
+            int parent1 = std::rand() % eliteSize;
+            int parent2 = std::rand() % eliteSize;
+            Chromosome child = crossover(elite[parent1], elite[parent2]);
+            if(std::rand() % 100 < 10){ //Implementing 10% chance to mutate child's path
+                mutate(child);
+            }
+            offspring.push_back(child);
+        }
         // Combine elite and offspring to create next generation
+        population = elite;
+        population.insert(population.end(), offspring.begin(), offspring.end());
     }
 
     //Display best solution after all generations
+    std::cout<<"Best path = ";
+    for (int i = 0; i < numCities; i++) {
+        std::cout << population[0].path[i] << ", ";
+    }
+
+    std::cout<< std::endl;
+    std::cout<< "Best distance = " << population[0].fitness <<std::endl;
     
     return 0;
 }
